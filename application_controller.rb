160c22ad40e264f07e21fc662dd0f7acd7a0e90b
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2017-06-15 20:53:34 +0000

    Kat and Fern KWK FINAL PROJECT

--- application_controller.rb
+++ application_controller.rb
@@ -7,9 +7,33 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
+  get '/hair' do
+    erb :hair
+  end  
+  
+  get '/skin' do
+    erb :skin
+  end  
 
-
-
-
+  post '/skin_results' do
+    skin_in=params[:skintype]
+    skin_out=params[:skinmoisture]
+    @skintype=skin_products(skin_in, skin_out)
+    @skin_product_name=@skintype[0]
+    @skin_product_image=@skintype[1]
+    @skin_product_description=@skintype[2]
+    erb :results
+  end
+  
+  post '/hair_results' do
+    hair_input=params[:type]
+    hair_output=params[:moisture]
+    @hairtype=hair_products(hair_input, hair_output)
+    @product_name=@hairtype[0]
+    @product_image=@hairtype[1]
+    @product_description=@hairtype[2]
+    erb :results
+  end
 
 end
+
